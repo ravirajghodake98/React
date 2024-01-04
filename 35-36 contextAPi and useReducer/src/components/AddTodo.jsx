fd2871c0a57ad38d5bcf1ffcf9eda7e227e212ed
Raviraj Ghodake <ravirajghodake98@gmail.com>
2024-01-04 18:35:10 +0530

    initial todo app

---
+++ 35-36 contextAPi and useReducer/src/components/AddTodo.jsx
@@ -0,0 +1,60 @@
+import { useState, useRef } from "react";
+import { BiMessageAdd } from "react-icons/bi";
+
+function AddTodo({ onNewItem }) {
+  // const [todoName, setTodoName] = useState('');
+  // const [dueDate, setDueDate] = useState('');
+  const todoNameElement = useRef();
+  const dueDateElement = useRef();
+
+  // const handleNameChange = (event) => {
+  //   setTodoName(event.target.value);
+  //   noOfUpdates.current += 1;
+  // }
+
+  // const handleDateChange = (event) => {
+  //   setDueDate(event.target.value);
+  //   console.log(`no. of updates are: ${noOfUpdates.current}`)
+  // }
+
+  const handleAddButtonClick = (event) => {
+    // console.log(event);
+    event.preventDefault();
+    const todoName = todoNameElement.current.value;
+    const dueDate = dueDateElement.current.value;
+    todoNameElement.current.value = "";
+    dueDateElement.current.value = "";
+    // console.log(`${todoName} due on: ${dueDate}`)
+    onNewItem(todoName, dueDate)
+    // setDueDate('');
+    // setTodoName('');
+  }
+
+  return (
+    <div className="container text-center">
+      <form className="row kg-row" onSubmit={handleAddButtonClick}>
+        <div className="col-6">
+          <input type="text" ref={todoNameElement} placeholder="Enter Todo Here"
+          // value={todoName}
+          // onChange={handleNameChange}
+          />
+        </div>
+        <div className="col-4">
+          <input type="date" ref={dueDateElement}
+          // value={dueDate}
+          // onChange={handleDateChange} 
+          />
+        </div>
+        <div className="col-2">
+          <button
+            className="btn btn-success kg-button"
+          >
+            <BiMessageAdd></BiMessageAdd>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default AddTodo;
---
+++ 35-36 contextAPi and useReducer/src/components/WelcomeMessage.jsx
@@ -0,0 +1,7 @@
+import styles from './WelcomeMessage.module.css'
+
+const WelcomeMessage = () => {
+  return <p className={styles.welcome}>Bahut hard!!!</p>
+}
+
+export default WelcomeMessage;
