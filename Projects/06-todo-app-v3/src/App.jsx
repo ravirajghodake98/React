89159c4d9ccd9b4bea658c28949e67def8e241d8
Raviraj Ghodake <ravirajghodake98@gmail.com>
2024-01-04 18:05:21 +0530

    updating state from previous state(functional updates)

--- Projects/06-todo-app-v3/src/App.jsx
+++ Projects/06-todo-app-v3/src/App.jsx
@@ -24,15 +24,38 @@ function App() {
 
   const handleNewItem = (itemName, itemDueDate) => {
     // console.log(`New item added: ${itemName} Date:${itemDueDate}`)
-    const newTodoItems = [
+
+    // Spread operator
+    /*const newTodoItems = [
       ...todoItems,
       {
         name: itemName,
         dueDate: itemDueDate,
       }
     ]
-    setTodoItems(newTodoItems);
-  }
+    setTodoItems(newTodoItems);*/
+
+    //using "method"
+    // setTodoItems((currValue) => {
+    //   const newTodoItems = [
+    //     ...currValue,
+    //     {
+    //       name: itemName,
+    //       dueDate: itemDueDate,
+    //     }
+    //   ]
+    //   return newTodoItems;
+    // });
+
+    //compact form of above code
+    setTodoItems((currValue) => [
+      ...currValue,
+      {
+        name: itemName,
+        dueDate: itemDueDate,
+      }
+    ]);
+  };
 
   const handleDeleteItem = (todoItemName) => {
     // console.log(`Item deleted: ${todoItemName}`)
