8b3b27c21b4bd380c2abb2cb09bbec94783e17b7
Raviraj Ghodake <ravirajghodake98@gmail.com>
2024-01-07 10:48:10 +0530

    useReducer

--- 35-36 contextAPi and useReducer/src/App.jsx
+++ 35-36 contextAPi and useReducer/src/App.jsx
@@ -3,42 +3,19 @@ import AddTodo from "./components/AddTodo";
 import TodoItems from "./components/TodoItems";
 import WelcomeMessage from "./components/WelcomeMessage";
 import "./App.css";
-import { useState } from "react";
-import { TodoItemsContext } from "./store/todo_item_store";
+import TodoItemsContextProvider from "./store/todo_item_store";
 
 function App() {
-  const [todoItems, setTodoItems] = useState([]);
-
-  const addNewItem = (itemName, itemDueDate) => {
-    setTodoItems((currValue) => [
-      ...currValue,
-      {
-        name: itemName,
-        dueDate: itemDueDate,
-      }
-    ]);
-  };
-
-  const deleteItem = (todoItemName) => {
-    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
-    setTodoItems(newTodoItems);
-  }
-
-  // const defaultTodoItems = [{ name: 'ghee', dueDate: 'today', }]
 
   return (
-    <TodoItemsContext.Provider value={{
-      todoItems,
-      addNewItem,
-      deleteItem
-    }}>
+    <TodoItemsContextProvider>
       <center className="todo-container">
         <AppName />
         <AddTodo />
         <WelcomeMessage></WelcomeMessage>
         <TodoItems ></TodoItems>
       </center>
-    </TodoItemsContext.Provider>
+    </TodoItemsContextProvider>
   );
 }
 
