333211abbc337c385097b13a83e457a529fbb86b
Raviraj Ghodake <ravirajghodake98@gmail.com>
2024-01-09 18:07:19 +0530

    post and postlist

--- Projects/07-social-media/src/App.jsx
+++ Projects/07-social-media/src/App.jsx
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Sidebar from './components/Sidebar';
+import CreatePost from './components/CreatePost';
+import PostList from './components/PostList';
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
       <Sidebar></Sidebar>
       <div className='content'>
         <Header></Header>
+        <CreatePost></CreatePost>
+        <PostList></PostList>
         <Footer></Footer>
       </div>
     </div>
