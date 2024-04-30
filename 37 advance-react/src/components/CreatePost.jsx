2db83dd4ad5f0130c38ebed26f5ee7180a2f7593
Raviraj Ghodake <ravirajghodake98@gmail.com>
2024-04-30 10:50:16 +0530

    copy of social media project

---
+++ 37 advance-react/src/components/CreatePost.jsx
@@ -0,0 +1,105 @@
+import { useContext, useRef } from "react";
+import { PostList } from "../store/post-list-store";
+
+const CreatePost = () => {
+  const { addPost } = useContext(PostList);
+
+  const userIdElement = useRef();
+  const postTitleElement = useRef();
+  const postBodyElement = useRef();
+  const reactionsElement = useRef();
+  const tagsElement = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const userId = userIdElement.current.value;
+    const postTitle = postTitleElement.current.value;
+    const postBody = postBodyElement.current.value;
+    const reactions = reactionsElement.current.value;
+    const tags = tagsElement.current.value.split(" ");
+
+    userIdElement.current.value = "";
+    postTitleElement.current.value = "";
+    postBodyElement.current.value = "";
+    reactionsElement.current.value = "";
+    tagsElement.current.value = "";
+
+    addPost(userId, postTitle, postBody, reactions, tags);
+  };
+
+  return (
+    <form className="create-post" onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="userId" className="form-label">
+          Enter your User Id
+        </label>
+        <input
+          type="text"
+          ref={userIdElement}
+          className="form-control"
+          id="userId"
+          placeholder="apni id daal bro"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="title" className="form-label">
+          Post Title
+        </label>
+        <input
+          type="text"
+          ref={postTitleElement}
+          className="form-control"
+          id="title"
+          placeholder="How you doing!!!"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="body" className="form-label">
+          Post Content
+        </label>
+        <textarea
+          type="text"
+          ref={postBodyElement}
+          rows="4"
+          className="form-control"
+          id="body"
+          placeholder="Aur bata bhai..."
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="reactions" className="form-label">
+          Number of reactions
+        </label>
+        <input
+          type="text"
+          ref={reactionsElement}
+          className="form-control"
+          id="reactions"
+          placeholder="kitne logo ne tere post pe muh banaya h"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="tags" className="form-label">
+          Enter your hashtags
+        </label>
+        <input
+          type="text"
+          ref={tagsElement}
+          className="form-control"
+          id="tags"
+          placeholder="space deke add kar tags"
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Post
+      </button>
+    </form>
+  );
+};
+
+export default CreatePost;
---
+++ 37 advance-react/src/store/post-list-store.jsx
@@ -0,0 +1,84 @@
+import { createContext, useReducer } from "react";
+import PropTypes from "prop-types";
+
+const DEFAULT_CONTEXT = {
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+};
+
+export const PostList = createContext(DEFAULT_CONTEXT);
+
+const postListReducer = (currPostList, action) => {
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter(
+      (post) => post.id !== action.payload.postId
+    );
+  } else if (action.type === "ADD_POST") {
+    newPostList = [action.payload, ...currPostList];
+  }
+
+  return newPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
+
+  const addPost = (userId, postTitle, postBody, reactions, tags) => {
+    dispatchPostList({
+      type: "ADD_POST",
+      payload: {
+        id: Date.now(),
+        title: postTitle,
+        body: postBody,
+        reactions,
+        userId,
+        tags,
+      },
+    });
+  };
+
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: {
+        postId,
+      },
+    });
+  };
+
+  return (
+    <PostList.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Welcome to my world",
+    body: "O bella ciao bella ciao bella ciao ciao ciao",
+    reactions: 5,
+    userId: "user-7",
+    tags: ["technology", "coding", "life"],
+  },
+  {
+    id: "2",
+    title: "chal nikal",
+    body: "kya bolti public",
+    reactions: 15,
+    userId: "user-12",
+    tags: ["one", "two", "three"],
+  },
+];
+
+PostListProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default PostListProvider;
