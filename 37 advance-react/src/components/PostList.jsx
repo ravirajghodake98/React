050f75d2e04393fd6485aed9eefefeb87167e323
Raviraj Ghodake <ravirajghodake98@gmail.com>
2024-05-21 00:19:42 +0530

    LoadingSpinner

--- 37 advance-react/src/components/PostList.jsx
+++ 37 advance-react/src/components/PostList.jsx
@@ -1,16 +1,21 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
   // const [dataFetched, setDataFetched] = useState(false);
+  const [fetching, setFetching] = useState(false);
+
   useEffect(() => {
+    setFetching(true);
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
+        setFetching(false);
       });
   }, []);
 
@@ -33,13 +38,12 @@ const PostList = () => {
 
   return (
     <>
-      {postList.length === 0 && (
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && (
         // <WelcomeMessage onGetPostsClick={handleGetPostsClick} />
         <WelcomeMessage />
       )}
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
     </>
   );
 };
