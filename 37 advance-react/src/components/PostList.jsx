25e5449080b47d0d873f78d4d52a9c5e360980a6
Raviraj Ghodake <ravirajghodake98@gmail.com>
2024-05-20 23:28:59 +0530

    fetch data with state method

--- 37 advance-react/src/components/PostList.jsx
+++ 37 advance-react/src/components/PostList.jsx
@@ -1,23 +1,34 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
+  const [dataFetched, setDataFetched] = useState(false);
 
-  const handleGetPostsClick = () => {
+  if (!dataFetched) {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
       });
-  };
+    setDataFetched(true);
+  }
+
+  // const handleGetPostsClick = () => {
+  // fetch("https://dummyjson.com/posts")
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     addInitialPosts(data.posts);
+  //   });
+  // };
 
   return (
     <>
       {postList.length === 0 && (
-        <WelcomeMessage onGetPostsClick={handleGetPostsClick} />
+        // <WelcomeMessage onGetPostsClick={handleGetPostsClick} />
+        <WelcomeMessage />
       )}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
--- 37 advance-react/src/components/WelcomeMessage.jsx
+++ 37 advance-react/src/components/WelcomeMessage.jsx
@@ -1,14 +1,15 @@
-const WelcomeMessage = ({ onGetPostsClick }) => {
+// const WelcomeMessage = ({ onGetPostsClick }) => {
+const WelcomeMessage = () => {
   return (
     <center className="welcome-message">
       <h1>There are no posts!</h1>
-      <button
+      {/* <button
         type="button"
         className="btn btn-primary"
         onClick={onGetPostsClick}
       >
         Get Posts From Server
-      </button>
+      </button> */}
     </center>
   );
 };
