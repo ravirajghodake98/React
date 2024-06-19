c4e37cdaf1de8a1989e0c0f3b1bc9406a03071f1
Raviraj Ghodake <ravirajghodake98@gmail.com>
2024-06-19 20:21:15 +0530

    add to bag functionality

--- Projects/09-myntra-clone/3-myntra-react/src/components/Header.jsx
+++ Projects/09-myntra-clone/3-myntra-react/src/components/Header.jsx
@@ -2,8 +2,11 @@ import { BsFillPersonFill } from "react-icons/bs";
 import { FaFaceGrinHearts } from "react-icons/fa6";
 import { BsBagCheckFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const bag = useSelector((store) => store.bag);
+
   return (
     <header>
       <div className="logo_container">
@@ -46,7 +49,7 @@ const Header = () => {
         <Link className="action_container" to="/bag">
           <BsBagCheckFill />
           <span className="action_name">Bag</span>
-          <span className="bag-item-count">0</span>
+          <span className="bag-item-count">{bag.length}</span>
         </Link>
       </div>
     </header>
--- Projects/09-myntra-clone/3-myntra-react/src/components/HomeItem.jsx
+++ Projects/09-myntra-clone/3-myntra-react/src/components/HomeItem.jsx
@@ -1,4 +1,21 @@
+import { useDispatch, useSelector } from "react-redux";
+import { bagActions } from "../store/bagSlice";
+import { GrAddCircle } from "react-icons/gr";
+import { MdDeleteForever } from "react-icons/md";
+
 const HomeItem = ({ item }) => {
+  const dispatch = useDispatch();
+  const bagItems = useSelector((store) => store.bag);
+  const elementFound = bagItems.indexOf(item.id) >= 0;
+
+  const handleAddToBag = () => {
+    dispatch(bagActions.addToBag(item.id));
+  };
+
+  const handleRemove = () => {
+    dispatch(bagActions.removeFromBag(item.id));
+  };
+
   return (
     <div className="item-container">
       <img className="item-image" src={item.image} alt="item image" />
@@ -12,12 +29,26 @@ const HomeItem = ({ item }) => {
         <span className="original-price">Rs {item.original_price}</span>
         <span className="discount">({item.discount_percentage}% OFF)</span>
       </div>
-      <button
-        className="btn-add-bag"
-        onClick={() => console.log("item clicked")}
-      >
-        Add to Bag
-      </button>
+
+      {elementFound ? (
+        <button
+          type="button"
+          className="btn btn-add-bag btn-danger"
+          onClick={handleRemove}
+        >
+          <MdDeleteForever />
+          &nbsp;Remove
+        </button>
+      ) : (
+        <button
+          type="button"
+          className="btn btn-add-bag btn-success"
+          onClick={handleAddToBag}
+        >
+          <GrAddCircle />
+          &nbsp;Add to Bag
+        </button>
+      )}
     </div>
   );
 };
